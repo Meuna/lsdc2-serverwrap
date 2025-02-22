d5f83f143fe9078037017b120f95e1723b9fbeaf
Thomas Bany <mun.bany@gmail.com>
2025-02-22 20:23:06 +0100

    Improved build scripts and move them to scripts folder

--- build/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-build_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker build -f $build_dir/Dockerfile $build_dir/.. -t lsdc2/serverwrap
-docker run --rm -v $build_dir:/mnt lsdc2/serverwrap
