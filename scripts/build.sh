54d4eb0d1d31d4d77c0fc1cba631f6354dc6cc6b
Thomas Bany <mun.bany@gmail.com>
2025-02-22 23:20:16 +0100

    Add go pkg caching

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,13 @@
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 src_dir=$script_dir/..
 
+mkdir -p $HOME/go/pkg
+
 podman build -f $script_dir/Dockerfile -t lsdc2/serverwrap:build-image $src_dir
 podman run \
     --rm \
     -v $src_dir:/go/src \
+    -v $HOME/go/pkg:/go/pkg \
     --workdir /go/src \
     lsdc2/serverwrap:build-image \
-    /bin/bash -c 'go get -u ./... && go build --ldflags "-w -s -extldflags \"-static\"" . '
+    /bin/bash -c 'go get ./... && go build --ldflags "-w -s -extldflags \"-static\"" . '
