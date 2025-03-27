ae0a0322becf831aa5df5fef0f52050f1454d8b2
Thomas Bany <mun.bany@gmail.com>
2025-03-28 00:22:28 +0100

    Autodetect EC2 and refactor configuration into Wrapped

--- cmd/serverwrap.go
+++ cmd/serverwrap.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -21,45 +20,35 @@ func main() {
 	}
 	defer logger.Sync() // flushes buffer, if any
 
-	cfg, err := internal.ParseEnv()
-	if err != nil {
-		logger.Panic("ParseEnv failed", zap.Error(err))
-	}
-	logger.Debug("configuration parsed", zap.Any("cfg", cfg))
+	// Initialise wrapped from command line and env
+	wrapped := internal.NewWrapped(logger, os.Args[1:])
+	logger.Debug("wrapped initialised", zap.Any("wrapped", wrapped))
 
 	// Prepare BPF to filter on incomming IP4 packes
-	filter := cfg.SniffFilter
-	if ip, err := internal.GetIP4(cfg.Iface); err == nil {
-		filter = fmt.Sprintf("dst host %v", ip)
-		if cfg.SniffFilter != "" {
-			filter = fmt.Sprintf("(%v) and (%v)", filter, cfg.SniffFilter)
-		}
-	}
-	logger.Debug("computed BPF filter", zap.String("filter", filter))
+	wrapped.UpdateFilterWithDestination()
 
-	// Prepare and wrapped start process
-	wrapped := internal.NewWrapped(logger, os.Args[1:], cfg)
-	logger.Debug("wrapped initialised")
-
+	// Start the process
 	wrapped.StartProcess()
 
 	// Start channel monitoring
 	pollingC := make(chan bool)
-	terminationCheckTicker := time.NewTicker(cfg.TerminationCheckDelay)
-	sniffTicker := time.NewTicker(cfg.SniffDelay)
-	emptyTicker := time.NewTicker(cfg.EmptyTimeout)
+	terminationCheckTicker := time.NewTicker(wrapped.TerminationCheckDelay)
+	sniffTicker := time.NewTicker(wrapped.SniffDelay)
+	emptyTicker := time.NewTicker(wrapped.EmptyTimeout)
 
 	sigC := make(chan os.Signal, 1)
 	signal.Notify(sigC, syscall.SIGTERM, syscall.SIGINT)
 
 	defer func() {
+		close(pollingC)
+		close(sigC)
 		terminationCheckTicker.Stop()
 		sniffTicker.Stop()
 		emptyTicker.Stop()
 		wrapped.StopProcess()
 	}()
 
-	if !cfg.InEc2Instance {
+	if !wrapped.InEc2Instance {
 		terminationCheckTicker.Stop()
 	}
 
@@ -69,36 +58,23 @@ func main() {
 		case packetFound := <-pollingC:
 			if packetFound {
 				logger.Debug("network activity detected")
-				emptyTicker.Reset(cfg.EmptyTimeout)
+				emptyTicker.Reset(wrapped.EmptyTimeout)
 			}
 		case <-sniffTicker.C:
 			go func() {
-				packetFound, err := internal.PollFilteredIface(cfg.Iface, filter, cfg.SniffTimeout)
-				if err != nil {
-					logger.Error("error polling network",
-						zap.String("iface", cfg.Iface),
-						zap.String("filter", filter),
-						zap.Error(err),
-					)
-					if cfg.PanicOnSocketError {
-						panic(err)
-					}
-				}
-				pollingC <- packetFound
+				pollingC <- wrapped.PollProcessPackets()
 			}()
 		case <-emptyTicker.C:
 			logger.Info("server empty for too long")
 			return
 		case <-terminationCheckTicker.C:
-			if cfg.InEc2Instance {
-				terminationNotified, err := internal.SpotTerminationIsNotified()
-				if err != nil {
-					logger.Error("error getting termination notification", zap.Error(err))
-					wrapped.NotifyBackend("🚫 Error worth checking in the EC2 instance")
-				}
-				if terminationNotified {
-					return
-				}
+			terminationNotified, err := internal.SpotTerminationIsNotified()
+			if err != nil {
+				logger.Error("error getting termination notification", zap.Error(err))
+				wrapped.NotifyBackend("🚫 Error worth checking in the EC2 instance")
+			}
+			if terminationNotified {
+				return
 			}
 		case <-sigC:
 			logger.Info("received signal")
